fd7660f574c8be99a51b8d95f505dfc8845d786f
jimbog <jimmygarzon@gmail.com>
2014-10-15 09:39:52 -0700

    update version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "easy unified handling of user interactions such as mouse, touch and pointer events",
-  version: "1.0.0",
+  version: "1.0.1",
   name: "jimbog:tap",
   git: "https://github.com/jimbog/tap.git",
 });
